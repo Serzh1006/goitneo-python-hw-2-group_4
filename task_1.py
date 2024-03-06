b97d0b85676f8d5a0cd82000024abe2cef8f2780
Sergey Kravchenko <sergey@MacBook-Pro-Sergey.local>
2024-03-06 09:38:22 +0100

    Добавлено методы для добавления, удаления и изменения контакта

--- task_1.py
+++ task_1.py
@@ -1,35 +1,65 @@
-from collections import defaultdict
-from datetime import datetime
+from collections import UserDict
 
-users_birthday = [{"name":'Audrey Williams',"birthday":datetime(1974,3,17)},
-{"name":'Anna Manning',"birthday":datetime(1996,3,5)},
-{"name":'Ray Green',"birthday":datetime(1986,3,10)},
-{"name":'Joseph Porter',"birthday":datetime(1997,3,1)},
-{"name":'Mary Nelson',"birthday":datetime(2000,7,15)},
-{"name":'Annie White',"birthday":datetime(2001,3,1)},
-{"name":'Susan Barker',"birthday":datetime(1991,3,2)},
-{"name":'Chad Johnson',"birthday":datetime(1979,3,6)},
-{"name":'Maria Simmons',"birthday":datetime(1993,3,3)},
-{"name":'Theodore Davis',"birthday":datetime(1995,3,4)}]
+class Field:
+    def __init__(self, value):
+        self.value = value
 
-def get_birthdays_per_week(users):
-    users_list_for_birthday = defaultdict(list)
-    today = datetime.today().date()
-    for date in users:
-        birthday = date['birthday'].date()
-        birthday_this_year = birthday.replace(year=today.year)
-        if birthday_this_year < today:
-            birthday_this_year = birthday.replace(year=today.year+1)
-        delta_days = (birthday_this_year - today).days
-        if delta_days < 7:
-            if birthday_this_year.weekday()==5 or birthday_this_year.weekday()==6:
-                users_list_for_birthday['Monday'].append(date['name'])
-            else:
-                weekday = birthday_this_year.strftime('%A')
-                users_list_for_birthday[weekday].append(date['name'])
-    return users_list_for_birthday
+    def __str__(self):
+        return str(self.value)
+
+class Name(Field):
+    def __init__(self, value):
+        super().__init__(value)
+
+class Phone(Field):
+    def __init__(self, value):
+        super().__init__(value)
 
 
-result = get_birthdays_per_week(users_birthday)
-for day,name in result.items():
-    print(f'{day}: {", ".join(name)}') 
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def add_phone(self,new_phone):
+        phone = Phone(new_phone)
+        self.phones.append(phone.value)
+
+    def remove_phone():
+        pass
+
+
+    def edit_phone(self,old,new):
+        index = self.phones.index(old)
+        self.phones[index] = new
+
+    def find_phone():
+        pass
+
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {'; '.join(p for p in self.phones)}"
+
+class AddressBook(UserDict):
+    def add_record(self,record):
+       self.data[record.name.value] = record
+
+    def find(self,name):
+        return self.data[name]
+
+    def delete(self,name):
+        self.data.pop(name)
+
+john_record = Record('John')
+book = AddressBook()
+alina_record = Record('Alina')
+john_record.add_phone('733101770')
+john_record.add_phone('1234567890')
+alina_record.add_phone('794325346')
+book.add_record(john_record)
+book.add_record(alina_record)
+
+john = book.find('John')
+john.edit_phone('733101770','101010101010')
+book.delete('John')
+book.delete('Alina')
+print(book.data)
